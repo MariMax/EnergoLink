8322b4694c2b6b5b7a73df3f7db707f952b36e90
MariMax <mma29121983@gmail.com>
2014-05-04 13:03:11 +0600

    Страница расчета

---
+++ FrontEnd/js/controllers/CalcCtrl.js
@@ -0,0 +1,6 @@
+define(['./module'], function (controllers) {
+    'use strict';
+    controllers.controller('CalcCtrl', [function ($scope) {
+    	
+    }]);
+});
--- FrontEnd/js/routes.js
+++ FrontEnd/js/routes.js
@@ -6,8 +6,8 @@
 define(['./app'], function (app) {
     'use strict';
     return app.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
+        $routeProvider.when('/CalcModule', {
+            templateUrl: 'partials/Calc.html',
             controller: 'MyCtrl1'
         });
 
@@ -22,7 +22,7 @@ define(['./app'], function (app) {
         });
 
         $routeProvider.otherwise({
-            redirectTo: '/view1'
+            redirectTo: '/CalcModule'
         });
     }]);
 });
